da26db63ab2d5c911d01d42c2af7712b55a273b4
JoseNavarroMiranda <nqvarr01234@gmail.com>
2026-08-14 18:30:15 -0600

    primer comentario

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('contact','contact')->name('contact');
+
+Route::view('blog', 'blog')->name('blog');
+
+Route::view('aboutUs', 'aboutUs')->name('aboutUs');
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+
+
+require __DIR__.'/auth.php';
